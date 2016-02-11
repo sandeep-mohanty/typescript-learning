21fb0ade559631925c05b6d2fd37c695804ffb36
sandeep-mohanty <access.sandeep@gmail.com>
2016-02-12 03:59:48 +0530

    Trivial changes

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,16 @@ export enum Lang {
 export class Language {
     static defaultName: string = Lang[4];
     private name: string;
-    private description: string = " is a programming language !"
+    private description: string = " is a programming language !";
 
     constructor(name: string = Lang[5], description: string) {
         this.name = name || Language.defaultName;
         if (description) this.description = description;
     }
 
-    getName(): string { return this.name }
+    getName():string {
+        return this.name;
+    }
+
     getDescription(): string { return this.description}
 }
