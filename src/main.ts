027985322210c0979dcea56e5956dd71d70f1e45
sandeep-mohanty <sandeep.mohanty@perceptivesoftware.com>
2016-02-11 21:52:41 +0530

    Added some script settings in the package.json to run some commands with NPM.

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export class Language {
     private name: string;
     private description: string = " is a programming language !"
 
-    constructor(name: string = Lang[5], description) {
+    constructor(name: string = Lang[5], description: string) {
         this.name = name || Language.defaultName;
         if (description) this.description = description;
     }
@@ -20,25 +20,3 @@ export class Language {
     getName(): string { return this.name }
     getDescription(): string { return this.description}
 }
-
-/*class FunctionalLanguage extends Language {
-    constructor(name: string = Lang[2], description: string) {
-        super(name, description);
-    }
-
-    getName(): string { return super.getName(); }
-}
-
-var p1: Language = new Language();
-var p2: Language = new FunctionalLanguage();
-var p3: Language = new Language(Lang[0]," belongs to the family of imperative programming languages.");
-var p4: Language = new FunctionalLanguage(Lang[1], " belongs to the family of functional programming languages.");
-
-//console.log("Proceduaral Language: " + p1.getName() + " !");
-console.log(`${p1.getName()}${p1.getDescription()}`);
-//console.log(p2.getName() + " !");
-console.log(`${p2.getName()}${p2.getDescription()}`);
-//console.log("Proceduaral Language: " + p3.getName() + " !");
-console.log(`${p3.getName()}${p3.getDescription()}`);
-//console.log(p4.getName() + " !");
-console.log(`${p4.getName()}${p4.getDescription()}`);*/
